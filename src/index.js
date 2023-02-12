3e2e39012654be89dd8fdbe3eb0031099eb99b3d
Lautaro Decima <decimalautaro@gmail.com>
2023-02-12 03:22:07 -0300

    configurando mongoose

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import {connect} from "mongoose"
 const app = express()
 
 
+const PORT = process.env.PORT;
+const NAME_DB = process.env.NAME_DB;
+
 const run = async ()=>{
-    await connect("mongodb://localhost:27017/" + "blog")
+    await connect("mongodb://localhost:27017/" + NAME_DB)
     console.log("conexion a la db exitosa.")
 
 
-    app.listen(3000, ()=>{
+    app.listen(PORT, ()=>{
         console.log("servidor escuchando en puerto 3000")
     })
 }
-
 
 run().catch((err)=> console.log(err))
