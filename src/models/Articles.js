598c6fa5a1036dff6b2e6f03275f72cd8158d3d3
Lautaro Decima <decimalautaro@gmail.com>
2023-02-12 22:37:28 -0300

    feat: crenado modelo de article

---
+++ src/models/Articles.js
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose'
+
+const ArticleSchema = Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+       
+    },
+    image:{
+        type: String,
+        default: "default.png"
+    }
+})
+
+module.exports = model ("Article", ArticleSchema)
