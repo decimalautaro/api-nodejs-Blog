e4b2a4fdd616fd65995c7e146d49054387197699
Lautaro Decima <decimalautaro@gmail.com>
2023-02-12 22:36:36 -0300

    fix: mofificando configuraciones

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import express from "express"
-import {connect} from "mongoose"
-import cors from 'cors'
+const express = require("express")
+const {connect} = require('mongoose')
+const cors = require("cors")
+require('dotenv').config();
+
+const {routerArticle} = require("./routes/article-router.js")
+
 
 const app = express()
 
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.use ("/api/articles", routerArticle)
+
 
 
 const PORT = process.env.PORT;
@@ -14,11 +24,11 @@ const NAME_DB = process.env.NAME_DB;
 const run = async ()=>{
     await connect("mongodb://localhost:27017/" + NAME_DB)
     console.log("conexion a la db exitosa.")
-
-
-    app.listen(PORT, ()=>{
-        console.log("servidor escuchando en puerto 3000")
-    })
 }
 
 run().catch((err)=> console.log(err))
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en: http://localhost:${PORT}`)
+})
