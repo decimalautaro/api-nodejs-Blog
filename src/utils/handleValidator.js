e597a1ad98039c744e42678c0d5cef861f40b31d
Lautaro Decima <decimalautaro@gmail.com>
2023-02-17 16:43:39 -0300

    feat: creando handleJWT, handlePassword, handleValidator

---
+++ src/utils/handleValidator.js
@@ -0,0 +1,13 @@
+const { validationResult } = require("express-validator");
+
+const validateResult = (req, res, next) =>{
+    try {
+        validationResult(req).throw();
+        return next();
+    } catch (error) {
+        res.status(400);
+        res.send({message: "faltan datos por enviar."});
+    }
+}
+
+module.exports = { validateResult }
