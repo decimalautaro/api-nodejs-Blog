379d767ba1c1581c32de684e9d54a7bcdd63909b
Lautaro Decima <decimalautaro@gmail.com>
2023-02-17 16:47:44 -0300

    fix: agregando encriptado a usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,24 +1,16 @@
 const User = require("../models/Users");
-const { validateUser } = require("../helpers/validate-user");
+const { encrypt, compare } = require ("../utils/handlePassword")
+const { tokenSign } = require("../utils/handleJWT")
 
-
-const create = (req, res) =>{
+const register = async(req, res) =>{
     const params = req.body;
+    const passwordHash = await encrypt(params.password);
+    const body = {...params , password:passwordHash};
+    
 
-    try{
-        
-        validateUser(params)
-
-    }catch (error) {
-        return res.status(400).json({
-            status:"error",
-            message: "faltan datos por enviar"
-        })
-    }
-
-    const user = new User(params);
-    user.save((error)=>{
-        if(error || !user){
+    const dataUser = await User.create(body);
+    dataUser.save((error)=>{
+        if(error || !dataUser){
             return res.status(404).json({
                 status:"error",
                 message: "no se ha guardado el usuario"
@@ -26,107 +18,19 @@ const create = (req, res) =>{
         }
     })
 
+    const data = {
+        token: await tokenSign(dataUser),
+        user: dataUser 
+    }
+
     return res.status(200).json({
         status: "success",
-        user: user,
+        user: data,
         message: "Usuario creado con exito"
 
     })
 }
 
-const getAll = (req, res) =>{
-    const consulta = User.find({}).sort({date:-1}).exec((error, users)=>{
-        if(error || !users){
-            return res.status(404).json({
-                status:"error",
-                message: "no se han encontrado usuarios"
-            })
-        }
-
-        return res.status(200).send({
-            status:"success",
-            users
-
-        })
-    })
-}
-
-const getById = (req, res)=>{
-    const {id} = req.params
-    User.findById(id, (error,user)=>{
-
-        if(error || !user){
-            return res.status(404).json({
-                status:"error",
-                message: "no se ha encontrado el usuario"
-            })
-        }
-        res.status(200).json({
-            status: "success",
-            user
-        })
-
-    })
-}
-const remove = (req, res)=>{
-    const { id } = req.params;
-    User.findOneAndDelete({_id : id}, (error,userRemove)=>{
-        if(error || !userRemove){
-            
-        return res.status(500).json({
-            status:"error",
-            user: userRemove,
-            message: "error al borrar el usuario"
-        })
-        }
-
-
-        return res.status(200).json({
-            status:"success",
-            user: userRemove,
-            message: "Usuario borrado"
-        })
-    }) 
-}
-
-const edit = (req, res) =>{
-    const { id } = req.params;
-    const params= req.body;
-
-    try{
-        
-        validateUser(params)
-        
-    }catch (error) {
-        return res.status(400).json({
-            status:"error",
-            message: "faltan datos por enviar"
-        })
-    }
-    
-    User.findOneAndUpdate({_id : id}, params, {new: true}, (error, userUpdate)=>{
-        
-        if(error || !userUpdate){
-            return res.status(500).json({
-                status: "error",
-                message: "error al actualizar el usuario"
-            })
-        }
-        
-        return res.status(200).json({
-            status: "success",
-            user: userUpdate
-        })
-
-    })
-
-
-}
-
 module.exports = {
-    create,
-    getAll,
-    getById,
-    remove,
-    edit
+    register,
 }
