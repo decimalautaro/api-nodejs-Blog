611b38f2a30c4864feb583d8cbd5f135b10371b2
Lautaro Decima <decimalautaro@gmail.com>
2023-02-19 01:08:53 -0300

    fix: usando express-validator en articulos

---
+++ src/validators/validate-article.js
@@ -0,0 +1,21 @@
+const { check } = require("express-validator");
+const {validateResult} = require("../utils/handleValidator")
+
+const validateArticle = [
+    check("title")
+    .exists()
+    .notEmpty()
+    .isLength({min:3, max:99}),
+    check("content")
+    .exists()
+    .notEmpty()
+    .isLength({min:3, max:2000}),
+    (req, res, next) => {
+        return validateResult(req, res, next)
+    }
+];
+
+
+module.exports = { validateArticle };
+
+
