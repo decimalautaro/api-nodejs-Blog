2f0b241575430829bd71e73245a785c2b131bbcb
Lautaro Decima <decimalautaro@gmail.com>
2023-02-18 22:26:28 -0300

    fix: usando express-validator

---
+++ src/validators/validate-user.js
@@ -0,0 +1,36 @@
+const { check } = require("express-validator");
+const {validateResult} = require("../utils/handleValidator")
+
+const validateRegister = [
+    check("name")
+    .exists()
+    .notEmpty()
+    .isLength({min:3, max:99}),
+    check("password")
+    .exists()
+    .notEmpty()
+    .isLength({min:3, max:15}),
+    check("email")
+    .exists()
+    .notEmpty()
+    .isEmail(),
+    (req, res, next) => {
+        return validateResult(req, res, next)
+    }
+];
+
+const validateLogin = [
+    check("password")
+    .exists()
+    .notEmpty()
+    .isLength({min:3, max:15}),
+    check("email")
+    .exists()
+    .notEmpty()
+    .isEmail(),
+    (req, res, next) => {
+        return validateResult(req, res, next)
+    }
+];
+
+module.exports = { validateRegister, validateLogin};
