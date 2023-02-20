9f94da5e21610683c9bd17eaa3414649b0e664c5
Lautaro Decima <decimalautaro@gmail.com>
2023-02-20 16:17:26 -0300

    feat: creando middleware de sesion de usuario

---
+++ src/middleware/session.js
@@ -0,0 +1,30 @@
+const { handleHttpError } = require("../utils/handleError")
+const { verifyToken } = require("../utils/handleJWT")
+const User = require("../models/Users")
+
+const authMIddleware = async(req, res, next)=>{
+    try {
+        
+        if (!req.headers.authorization) {
+            handleHttpError(res, "Se necesita una sesión.", 401);
+            return
+        }
+
+        const token = req.headers.authorization.split(" ").pop();
+        const dataToken  = await verifyToken(token);
+        
+        if (!dataToken._id) {
+            handleHttpError(res, "Error en el ID del token.",401);
+            return
+        }
+        const user = await User.findById(dataToken._id);
+        req.user = user
+
+        next();
+
+    } catch (error) {
+        handleHttpError(res, "Sesion no autorizada", 401);
+    }
+}
+
+module.exports = { authMIddleware }
