1a10552ff1121f87b1fa9c8d6d9918f8ef5ff41d
Lautaro Decima <decimalautaro@gmail.com>
2023-02-21 18:44:17 -0300

    feat: protegiendo rutas mediante roles

---
+++ src/middleware/rol.js
@@ -0,0 +1,24 @@
+const { handleHttpError } = require("../utils/handleError")
+
+const checkRol = (rols)=> (req, res, next) =>{
+    try {
+        const { user } = req;
+        console.log({user})
+        const rolesByUser = user.role;
+
+        checkValueRol = rols.some((rolSigngle)=> rolesByUser.includes(rolSigngle));
+        if(!checkValueRol){
+            handleHttpError(res, "Error el usuario no tiene permisos.", 403);   
+            return 
+        }
+
+        next();
+
+    } catch (error) {
+        handleHttpError(res, "Error de permisos.", 403);   
+    }
+}
+
+module.exports = {
+    checkRol
+}
